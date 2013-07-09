d35ed6c2976dd9f88cacc8cfa4fe17604514e8cd
Harris Hancock <vortrab@gmail.com>
2013-07-08 17:39:38 -0700

    Un-C99 net_byte_order.c for now

--- net_byte_order.c
+++ net_byte_order.c
@@ -1,24 +1,34 @@
 #include "net_byte_order.h"
 
-#define BYTESWAP(X) \
-    ({ \
-        __typeof__(X) result; \
-        uint8_t *presult = (uint8_t *)&result; \
-        for (int i = sizeof(X) - 1; i >= 0; --i) { \
-            presult[i] = (X) & 0xff; \
-            (X) >>= 8; \
-        } \
-        result; \
-     })
-
 uint16_t netByteOrder16 (uint16_t value) {
-    return BYTESWAP(value);
+    int i;
+    uint16_t result;
+    uint8_t *presult = (uint8_t *)&result;
+    for (i = sizeof(value) - 1; i >= 0; --i) {
+        presult[i] = value & 0xff;
+        value >>= 8;
+    }
+    return result;
 }
 
 uint32_t netByteOrder32 (uint32_t value) {
-    return BYTESWAP(value);
+    int i;
+    uint32_t result;
+    uint8_t *presult = (uint8_t *)&result;
+    for (i = sizeof(value) - 1; i >= 0; --i) {
+        presult[i] = value & 0xff;
+        value >>= 8;
+    }
+    return result;
 }
 
 uint64_t netByteOrder64 (uint64_t value) {
-    return BYTESWAP(value);
+    int i;
+    uint64_t result;
+    uint8_t *presult = (uint8_t *)&result;
+    for (i = sizeof(value) - 1; i >= 0; --i) {
+        presult[i] = value & 0xff;
+        value >>= 8;
+    }
+    return result;
 }
